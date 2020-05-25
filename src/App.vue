32f376f15d53271ff94ffce61d7b951af8544f02
Macbook <macbook@MacbookdeMacBook-Pro.local>
2020-05-25 19:21:00 +0800

    Change App.vue AND add Cart.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>
+    <h2>FAKE Taobao</h2>
+    <ul>
+      <li v-for="(good,index) in goods" :key="index">
+        {{good.text}}
+         | 
+         {{good.price}}
+         <button @click="addCart(index)">添加购物车</button>
+      </li>
+    </ul>
+    <Cart></Cart>
+  </div>  
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Cart } from "@/components/Cart"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  data(){
+    return{
+      goods:[
+        {text:"语文", price:50},
+        {text:"数学", price:60},
+        {text:"英语", price:70}
+      ],
+      cart:[]
+    }
+  },
+  methods:{
+    onReduce(arg){   
+      let {index} = arg
+      if (this.cart[index].count > 1) {
+        this.cart[index].count -= 1
+      }
+    },
+    onAdd(arg){
+      let {index} = arg
+      this.cart[index].count += 1
+    },
+    addCart(i){
+      console.log('添加购物车')
+      let item = this.goods[i]
+      let good = this.cart.find(v => v.text== item.text)
+      if(good){
+        good.count += 1
+      } else {
+        this.cart.push({...item, count:1})
+      }
+    }
+    
+  },
+  components:{
+    Cart
   }
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
---
+++ src/components/Cart.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <h2>购物车</h2>
+
+      <table border="1">
+          <tr>
+              <td>商品</td>
+              <td>单价</td>
+              <td>价格</td>
+              <td>总量</td>
+              <td>操作</td>
+          </tr>
+
+          <tr v-for="(c,index) in data" :key="c.index">
+              <td>{{c.text}}</td>
+              <td>{{c.price}}</td>
+              <td>{{c.price * c.count}}</td>
+              <td>{{c.count}}</td>
+              <td>
+                  <button @click="add(index)">+</button>
+                  <button @click="reduce(index)">-</button>
+              </td>
+          </tr>
+      </table>
+  </div>
+</template>
+
+<script>
+export default{
+    name: 'Cart',
+    props:['data'],
+    methods:{
+        add(i){
+            this.$emit('add', {index:i})
+        },
+        reduce(i){
+            this.$emit('reduce',{index: i})
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
